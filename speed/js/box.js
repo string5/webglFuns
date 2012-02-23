0e46c3ea3a08e654a53ae1a1d595312aa6f4dc95
string5 <cys12345@gmail.com>
2012-02-24 06:17:25 +0800

    实例化

---
+++ speed/js/box.js
@@ -0,0 +1,180 @@
+﻿//========================================================================//
+//      Copyright 2011, Smilodon Studio Inc.
+//      All rights reserved.
+//========================================================================//
+
+/**
+ *	box
+ *	@author: CYS
+ */
+var box = function()
+{
+	this.posBuffer = null;
+	this.tcBuffer  = null;
+	this.indexBuffer = null;
+	this.effCube = null;
+};
+
+
+box.prototype.initBuffers = function()
+{
+	var vertices = 
+		[  
+			// Front face
+			-1.0, -1.0,  1.0,
+			1.0, -1.0,  1.0,
+			1.0,  1.0,  1.0,
+			-1.0,  1.0,  1.0,
+
+			// Back face
+			-1.0, -1.0, -1.0,
+			-1.0,  1.0, -1.0,
+			1.0,  1.0, -1.0,
+			1.0, -1.0, -1.0,
+
+			// Top face
+			-1.0,  1.0, -1.0,
+			-1.0,  1.0,  1.0,
+			1.0,  1.0,  1.0,
+			1.0,  1.0, -1.0,
+
+			// Bottom face
+			-1.0, -1.0, -1.0,
+			1.0, -1.0, -1.0,
+			1.0, -1.0,  1.0,
+			-1.0, -1.0,  1.0,
+
+			// Right face
+			1.0, -1.0, -1.0,
+			1.0,  1.0, -1.0,
+			1.0,  1.0,  1.0,
+			1.0, -1.0,  1.0,
+
+			// Left face
+			-1.0, -1.0, -1.0,
+			-1.0, -1.0,  1.0,
+			-1.0,  1.0,  1.0,
+			-1.0,  1.0, -1.0, 
+		];
+
+	this.posBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, this.posBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+	this.posBuffer.itemSize = 3;
+	this.posBuffer.numItems = 24;
+
+	var texCoords = 
+		[
+			// Front face
+			0.0, 0.0,
+			1.0, 0.0,
+			1.0, 1.0,
+			0.0, 1.0,
+
+			// Back face
+			1.0, 0.0,
+			1.0, 1.0,
+			0.0, 1.0,
+			0.0, 0.0,
+
+			// Top face
+			0.0, 1.0,
+			0.0, 0.0,
+			1.0, 0.0,
+			1.0, 1.0,
+
+			// Bottom face
+			1.0, 1.0,
+			0.0, 1.0,
+			0.0, 0.0,
+			1.0, 0.0,
+
+			// Right face
+			1.0, 0.0,
+			1.0, 1.0,
+			0.0, 1.0,
+			0.0, 0.0,
+
+			// Left face
+			0.0, 0.0,
+			1.0, 0.0,
+			1.0, 1.0,
+			0.0, 1.0,
+		];
+
+	this.tcBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, this.tcBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texCoords), gl.STATIC_DRAW);
+	this.tcBuffer.itemSize = 2;
+	this.tcBuffer.numItems = 24;
+
+	var cubeIndices = 
+		[
+			0, 1, 2,      0, 2, 3,    // Front face
+			4, 5, 6,      4, 6, 7,    // Back face
+			8, 9, 10,     8, 10, 11,  // Top face
+			12, 13, 14,   12, 14, 15, // Bottom face
+			16, 17, 18,   16, 18, 19, // Right face
+			20, 21, 22,   20, 22, 23  // Left face
+		];
+
+	this.indexBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeIndices), gl.STATIC_DRAW);
+	this.indexBuffer.itemSize = 1;
+	this.indexBuffer.numItems = 36;
+
+}
+
+
+box.prototype.initShader = function()
+{
+	this.effCube = new program();
+	this.effCube.loadFromID("vsCube");
+	this.effCube.loadFromID("psCube");
+	this.effCube.bind();
+
+	this.effCube.pos = gl.getAttribLocation(this.effCube.program, "aPos");
+	gl.enableVertexAttribArray(this.effCube.pos);
+
+	this.effCube.tc = gl.getAttribLocation(this.effCube.program, "aTc");
+	gl.enableVertexAttribArray(this.effCube.tc);
+
+	this.effCube.time = gl.getUniformLocation( this.effCube.program, 'time' );
+	this.effCube.resolution = gl.getUniformLocation( this.effCube.program, 'resolution' );
+	this.effCube.mouse = gl.getUniformLocation( this.effCube.program, 'mouse' );
+	this.effCube.matMVP = gl.getUniformLocation( this.effCube.program, 'matMVP' );
+}
+
+
+box.prototype.init = function()
+{
+	this.initBuffers();
+	this.initShader();
+}
+
+box.prototype.set = function()
+{
+	gl.uniform1f( this.effCube.time, timer.time/1000 );
+	gl.uniform2f( this.effCube.resolution, canvas.width, canvas.height );
+	gl.uniform2f( this.effCube.mouse, mouse.x, mouse.y);
+}
+
+
+box.prototype.setMVP = function(mvp)
+{
+	gl.uniformMatrix4fv(this.effCube.matMVP, false, mvp.elements);
+}
+
+
+box.prototype.draw = function()
+{
+	gl.bindBuffer(gl.ARRAY_BUFFER, this.posBuffer);
+	gl.vertexAttribPointer(this.effCube.pos, this.posBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	gl.bindBuffer(gl.ARRAY_BUFFER, this.tcBuffer);
+	gl.vertexAttribPointer(this.effCube.tc, this.tcBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+	gl.drawElements(gl.TRIANGLES, this.indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+
+}
--- speed/js/game.js
+++ speed/js/game.js
@@ -16,6 +16,7 @@ var infoPad;
 var timer = {time: 0, dtime: 0, startime: Date.now(), lasttime: Date.now() };
 var mouse = {x: 0, y: 0, down: false};
 
+var cubeTest;
 
 // ----------------------------------------------------------------------------
 // initlalize
@@ -56,148 +57,13 @@ function init()
 	infoPad = new infoPad();
 	infoPad.add("Speed game", "@Smilodon Studio");
 
-	initBuffers();
-	initShaders();
+	cubeTest = new box();
+	cubeTest.init();
 }
 
 
 // ----------------------------------------------------------------------------
 // scene
-
-var posBuffer;
-var tcBuffer;
-var indexBuffer;
-
-function initBuffers()
-{
-	var vertices = 
-	[  
-		// Front face
-		-1.0, -1.0,  1.0,
-		1.0, -1.0,  1.0,
-		1.0,  1.0,  1.0,
-		-1.0,  1.0,  1.0,
-
-		// Back face
-		-1.0, -1.0, -1.0,
-		-1.0,  1.0, -1.0,
-		1.0,  1.0, -1.0,
-		1.0, -1.0, -1.0,
-
-		// Top face
-		-1.0,  1.0, -1.0,
-		-1.0,  1.0,  1.0,
-		1.0,  1.0,  1.0,
-		1.0,  1.0, -1.0,
-
-		// Bottom face
-		-1.0, -1.0, -1.0,
-		1.0, -1.0, -1.0,
-		1.0, -1.0,  1.0,
-		-1.0, -1.0,  1.0,
-
-		// Right face
-		1.0, -1.0, -1.0,
-		1.0,  1.0, -1.0,
-		1.0,  1.0,  1.0,
-		1.0, -1.0,  1.0,
-
-		// Left face
-		-1.0, -1.0, -1.0,
-		-1.0, -1.0,  1.0,
-		-1.0,  1.0,  1.0,
-		-1.0,  1.0, -1.0, 
-	];
-
-	posBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-	posBuffer.itemSize = 3;
-	posBuffer.numItems = 24;
-
-	var texCoords = 
-	[
-		// Front face
-		0.0, 0.0,
-		1.0, 0.0,
-		1.0, 1.0,
-		0.0, 1.0,
-
-		// Back face
-		1.0, 0.0,
-		1.0, 1.0,
-		0.0, 1.0,
-		0.0, 0.0,
-
-		// Top face
-		0.0, 1.0,
-		0.0, 0.0,
-		1.0, 0.0,
-		1.0, 1.0,
-
-		// Bottom face
-		1.0, 1.0,
-		0.0, 1.0,
-		0.0, 0.0,
-		1.0, 0.0,
-
-		// Right face
-		1.0, 0.0,
-		1.0, 1.0,
-		0.0, 1.0,
-		0.0, 0.0,
-
-		// Left face
-		0.0, 0.0,
-		1.0, 0.0,
-		1.0, 1.0,
-		0.0, 1.0,
-	];
-
-	tcBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, tcBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texCoords), gl.STATIC_DRAW);
-	tcBuffer.itemSize = 2;
-	tcBuffer.numItems = 24;
-
-	var cubeIndices = 
-	[
-		0, 1, 2,      0, 2, 3,    // Front face
-		4, 5, 6,      4, 6, 7,    // Back face
-		8, 9, 10,     8, 10, 11,  // Top face
-		12, 13, 14,   12, 14, 15, // Bottom face
-		16, 17, 18,   16, 18, 19, // Right face
-		20, 21, 22,   20, 22, 23  // Left face
-	];
-
-	indexBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeIndices), gl.STATIC_DRAW);
-	indexBuffer.itemSize = 1;
-	indexBuffer.numItems = 36;
-
-}
-
-
-var effFlower;
-function initShaders()
-{
-	effFlower = new program();
-	effFlower.loadFromID("shader-fs");
-	effFlower.loadFromID("shader-vs");
-	effFlower.bind();
-
-	effFlower.pos = gl.getAttribLocation(effFlower.program, "aPos");
-	gl.enableVertexAttribArray(effFlower.pos);
-
-	effFlower.tc = gl.getAttribLocation(effFlower.program, "aTc");
-	gl.enableVertexAttribArray(effFlower.tc);
-
-	effFlower.time = gl.getUniformLocation( effFlower.program, 'time' );
-	effFlower.resolution = gl.getUniformLocation( effFlower.program, 'resolution' );
-	effFlower.mouse = gl.getUniformLocation( effFlower.program, 'mouse' );
-	effFlower.matMVP = gl.getUniformLocation( effFlower.program, 'matMVP' );
-}
 
 function drawScene()
 {
@@ -206,25 +72,25 @@ function drawScene()
 	gl.enable(gl.DEPTH_TEST);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 	
-	
+	cubeTest.set();
+
 	var axis = new vec3(1, 1, 1);
+	var matP = mat4.makePerspective(70, gl.viewportWidth/gl.viewportHeight, 0.1, 5000);
 	var matT = mat4.makeTranslate(0, 0, -5);
 	var matR = mat4.makeRotate(timer.time*0.001, axis);
-	var matMVP = mat4.makePerspective(70, gl.viewportWidth/gl.viewportHeight, 0.1, 5000);
-	matMVP.x(matT).x(matR);
+	var matMVP = new mat4();
+	matMul(matMVP, matP, matT.x(matR) );
 
-	gl.uniform1f( effFlower.time, timer.time/1000 );
-	gl.uniform2f( effFlower.resolution, canvas.width, canvas.height );
-	gl.uniform2f( effFlower.mouse, mouse.x, mouse.y);
-	gl.uniformMatrix4fv(effFlower.matMVP, false, matMVP.elements);
+	cubeTest.setMVP(matMVP);
+	cubeTest.draw();
 
-	gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer);
-	gl.vertexAttribPointer(effFlower.pos, posBuffer.itemSize, gl.FLOAT, false, 0, 0);
-	gl.bindBuffer(gl.ARRAY_BUFFER, tcBuffer);
-	gl.vertexAttribPointer(effFlower.tc, tcBuffer.itemSize, gl.FLOAT, false, 0, 0);
-
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-	gl.drawElements(gl.TRIANGLES, indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+	axis = new vec3(1, 0.1, 1);
+	matT = mat4.makeTranslate(2, 3, -10);
+	matR = mat4.makeRotate(timer.time*0.0013, axis);
+	matMul(matMVP, matP, matT.x(matR) );
+	
+	cubeTest.setMVP(matMVP);
+	cubeTest.draw();
 
 	infoPad.show();
 
